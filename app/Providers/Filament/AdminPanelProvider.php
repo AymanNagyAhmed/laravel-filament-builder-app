ceeec74d90d2898e9ce83ce13475d8497a7def59
Aymannagyahmed <aymannagy.ahmed@gmail.com>
2023-11-13 16:45:06 +0200

    change logo 2 methods

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -37,6 +37,8 @@ class AdminPanelProvider extends PanelProvider
                 'warning' => Color::Orange,
             ])
             ->font('Poppins')
+            // ->brandName('')
+            // ->brandLogo(asset('images/logo-transparent.png'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
---
+++ resources/views/vendor/filament-panels/components/logo.blade.php
@@ -0,0 +1,2 @@
+
+<img src="{{ asset('images/logo-transparent.png') }}" alt="logo" class="h-10">
